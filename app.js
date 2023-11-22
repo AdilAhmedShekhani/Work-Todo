d01eb76200ccfc96afe94008a06f654f1acec170
AdilAhmedShekhani <adilaziz2009@live.com>
2023-11-22 13:26:19 +0500

    Add

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ inputBx.addEventListener("keyup", function (event) {
 });
 
 let addItem = (inputBx) => {
-  let listItem = document.createElement("li");
-  listItem.innerHTML = `${inputBx}<i>`;
+  if (inputBx.trim() !== "") {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputBx}<i>`;
 
-  listItem.addEventListener("click", function () {
-    this.classList.toggle("done");
-  });
+    listItem.addEventListener("click", function () {
+      this.classList.toggle("done");
+    });
 
-  listItem.querySelector("i").addEventListener("click", function () {
-    listItem.remove();
-  });
+    listItem.querySelector("i").addEventListener("click", function () {
+      listItem.remove();
+    });
 
-  list.appendChild(listItem);
+    list.appendChild(listItem);
+  }
 };
